77453ac21fc0a5b6b9b918183f969a8137816ab5
Никита Рыбаковский <narybakovskiy@edu.hse.ru>
2024-05-10 17:20:08 +0300

    fix

--- backend/app/api/api_v1/routers/train_nlp.py
+++ backend/app/api/api_v1/routers/train_nlp.py
@@ -9,6 +9,27 @@ import pickle
 train_nlp_router = r = APIRouter()
 
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_text(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def send_json(self, message: dict, websocket: WebSocket):
+        await websocket.send_json(message)
+
+
+manager = ConnectionManager()
+
+
 @r.get("/")
 async def get():
     return {200: "OK"}
@@ -50,3 +71,18 @@ async def predict_classification(json_data: dict, user_id: str):
         "result": prediction,
         }
 
+
+@r.websocket("/ws/classification/{user_id}")
+async def classification(websocket: WebSocket, user_id):
+    await manager.connect(websocket)
+    try:
+        text_clf = pickle.loads(storage.get_object(f"user_{user_id}/text_clf.pt"))
+        while True:
+            data = await websocket.receive()
+            text = data.get("text")
+            prediction = text_clf.predict(txt)
+            print(prediction)
+
+            await manager.send_json(prediction, websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,14 +2,12 @@ from fastapi import FastAPI, Depends
 from starlette.requests import Request
 import uvicorn
 
-# from natural_language_processing.trainable_models.text_classification import TextClassification
-
 from api.api_v1.routers.users import users_router
 from api.api_v1.routers.auth import auth_router
 from api.api_v1.routers.pre_trained import cv_router
 from api.api_v1.routers.train import train_router
 from api.api_v1.routers.train_nlp import train_nlp_router
-# from api.api_v1.routers.pre_trained_nlp import nlp_router
+from api.api_v1.routers.pre_trained_nlp import nlp_router
 from core import config
 from db.session import SessionLocal
 from core.auth import get_current_active_user
@@ -51,7 +49,7 @@ app.include_router(
 )
 app.include_router(auth_router, prefix="/api", tags=["auth"])
 app.include_router(cv_router, prefix="/api/cv", tags=["cv"])
-# app.include_router(nlp_router, prefix="/api/nlp", tags=["nlp"])
+app.include_router(nlp_router, prefix="/api/nlp", tags=["nlp"])
 app.include_router(train_router, prefix="/api/cv/train", tags=["train"])
 app.include_router(train_nlp_router, prefix="/api/nlp/train", tags=["train"])
 
@@ -62,5 +60,4 @@ def test_json(req: JSONValidation):
 
 
 if __name__ == "__main__":
-    # TextClassification = TextClassification
     uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8888)
