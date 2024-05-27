98e4f039cf22a02446d55477ff5e0108998919fd
Никита Рыбаковский <narybakovskiy@edu.hse.ru>
2024-05-27 23:47:24 +0300

    all models

--- backend/app/api/api_v1/routers/downloads.py
+++ backend/app/api/api_v1/routers/downloads.py
@@ -20,7 +20,27 @@ def download_cv_classification(user_id: str):
 def download_cv_face_recognition(user_id: str):
   model_rec = storage.get_object(f"user_{user_id}/face-recognition/model_rec.pkl")
   if model_rec:
-    # stream = io.BytesIO(model_rec)
     return Response(model_rec)
   else:
     return {500: "error"}
+
+
+@r.get("/cv/detection/{user_id}")
+def download_cv_detection(user_id: str):
+  with open(f"./app/computer_vision/resources/user_{user_id}/train/weights/best.pt", "rb") as f:
+    model = f.read()
+  
+  if model:
+    return Response(model)
+  else:
+    return {500: "error"}
+
+
+@r.get("/nlp/classification/{user_id}")
+def download_nlp_classification(user_id: str):
+  model = pickle.loads(storage.get_object(f"user_{user_id}/text_clf.pt"))
+  if model:
+    return Response(model)
+  else:
+    return {500: "error"}
+  
